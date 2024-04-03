ea80d4169252a601ac7acb03deeec2f874831584
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-04-03 15:44:42 +0700

    commit worldwise project

---
+++ 11-worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <p>Page Not Found</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
