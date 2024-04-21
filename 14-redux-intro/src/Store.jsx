02ba7dff0e8e508478e04a5cc5ddddb12d9f789d
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-04-21 19:19:02 +0700

    feat: create store reducer

---
+++ 14-redux-intro/src/Store.jsx
@@ -0,0 +1,86 @@
+import { createStore } from "redux";
+
+const initialsState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialsState, action) {
+  switch (action.type) {
+    case "account/deposit": {
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    }
+    case "account/withdraw": {
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    }
+    case "account/requestLoan": {
+      if (state.loan > 0) {
+        return {
+          ...state,
+        };
+      }
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.amount,
+      };
+    }
+    case "account/payLoan": {
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+// const store = createStore(reducer);
+// store.dispatch({ type: "account/deposit", payload: 100 });
+// console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 30 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 100, loanPurpose: "buy a car" },
+// });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/payLoan",
+// });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return {
+    type: "account/deposit",
+    payload: amount,
+  };
+}
+function withdraw(amount) {
+  return {
+    type: "account/withdraw",
+    payload: amount,
+  };
+}
+function requestLoan(amount, loanPurpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, loanPurpose },
+  };
+}
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
