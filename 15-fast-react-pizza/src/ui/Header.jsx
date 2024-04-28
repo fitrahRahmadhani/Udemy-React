a06925d6625b9ed3fb4f1442c2111d12939e28a4
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-04-28 12:16:21 +0700

    feat: add feature create new data order

--- 15-fast-react-pizza/src/ui/Header.jsx
+++ 15-fast-react-pizza/src/ui/Header.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 function header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
       <p>Jonas</p>
     </header>
   );
