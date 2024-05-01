5690a3bc719e269ee61e45e5ef5b0e3a2f59fe6d
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-05-01 18:03:09 +0700

    feat: styling with tailwindcss

--- 15-fast-react-pizza/src/ui/Home.jsx
+++ 15-fast-react-pizza/src/ui/Home.jsx
@@ -1,13 +1,25 @@
+import { useSelector } from "react-redux";
+import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 function Home() {
+  const user = useSelector((state) => state.user.username);
   return (
-    <div>
-      <h1 className="text-center text-xl font-semibold text-stone-700">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
         </span>
       </h1>
+
+      {user === "" ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering, {user}
+        </Button>
+      )}
     </div>
   );
 }
