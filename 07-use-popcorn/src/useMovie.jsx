247cf3cf2d2681097d1b3b3a9c1f0b8f97a207de
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-05-16 23:46:00 +0700

    fix: fix some bug when fetch data from API

--- 07-use-popcorn/src/useMovie.jsx
+++ 07-use-popcorn/src/useMovie.jsx
@@ -1,50 +1,56 @@
 import { useState, useEffect } from "react";
+
 const KEY = "5bd87cb6";
+
 export function useMovies(query, callback) {
   const [movies, setMovies] = useState([]);
-  const [isLodiang, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); // Perbaikan typo
   const [error, setError] = useState("");
-  useEffect(
-    function () {
-      callback?.();
-      const controller = new AbortController();
-      async function fetchMovie() {
-        try {
-          setIsLoading(true);
-          setError("");
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
 
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movie");
+  useEffect(() => {
+    callback?.();
 
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
+    const controller = new AbortController();
 
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.message !== "AbortError") {
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      if (query.length < 3) {
-        setMovies([]);
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
         setError("");
-        return;
+
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal }
+        );
+
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching movie");
+
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("Movie not found");
+
+        setMovies(data.Search);
+        setError("");
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
+      } finally {
+        setIsLoading(false);
       }
-      // handleCloseSelectedMovie();
-      fetchMovie();
-      return function () {
-        controller.abort();
-      };
-    },
-    [query, callback]
-  );
-  return { movies, isLodiang, error };
+    }
+
+    if (query.length < 3) {
+      setMovies([]);
+      setError("");
+      return;
+    }
+
+    fetchMovies();
+
+    return () => {
+      controller.abort();
+    };
+  }, [query, callback]);
+
+  return { movies, isLoading, error };
 }
