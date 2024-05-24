12fc4dfb5ec8635d4e0aab889b7d9a1956a0d06d
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-05-24 23:44:03 +0700

    feat(dashboard): add data filter

--- 16-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 16-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,29 @@
 import styled from "styled-components";
-
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isPending, bookings } = useRecentBookings();
+  const { isPending: isPendingStays, stays, confirmedStays } = useRecentStays();
+
+  if (isPending || isPendingStays) return <Spinner />;
+
+  console.log(bookings);
+  console.log(stays);
+  console.log(confirmedStays);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Test</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
