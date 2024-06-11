f4d4f0477eea620fee8839aeaad0e6c459604d32
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-06-11 20:28:32 +0700

    init project the wild oasis

---
+++ 21-the-wild-oasis-website/app/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function Page() {
+  return (
+    <div>
+      <h1>The Wild Oasis, welcone to paradise</h1>
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
+}
