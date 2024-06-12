bef46a06597af547660e24ca74e88e781b9c99bf
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-06-12 22:27:24 +0700

    Setup layout, meta, tailwindcss

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,20 +1,29 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description: "Luxurious Cabins in Los Angeles California USA",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto bg-red-500">{children}</main>
+        </div>
       </body>
     </html>
   );
