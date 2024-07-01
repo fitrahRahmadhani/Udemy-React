43b5cfa57c1419f5e9f3ecfc2df49128be24e8f0
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-07-01 17:54:13 +0700

    feat(cabins): add filter cabin capacity

--- 21-the-wild-oasis-website/app/_components/CabinList.js
+++ 21-the-wild-oasis-website/app/_components/CabinList.js
@@ -2,15 +2,24 @@ import { unstable_noStore as noStore } from "next/cache";
 import { getCabins } from "@/app/_lib/data-service";
 import CabinCard from "@/app/_components/CabinCard";
 
-async function CabinList() {
-  noStore();
+async function CabinList({ filter }) {
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  let displayCabin;
+  if (filter === "all") displayCabin = cabins;
+  if (filter === "small")
+    displayCabin = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  if (filter === "medium")
+    displayCabin = cabins.filter(
+      (cabin) => cabin.maxCapacity > 3 && cabin.maxCapacity <= 7
+    );
+  if (filter === "large")
+    displayCabin = cabins.filter((cabin) => cabin.maxCapacity > 7);
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayCabin.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
-import CabinList from "../_components/CabinList";
-import Spinner from "../_components/Spinner";
+import CabinList from "@/app/_components/CabinList";
+import Spinner from "@/app/_components/Spinner";
+import Filter from "@/app/_components/Filter";
 
 export const metadata = {
   title: "Cabins",
@@ -8,7 +9,8 @@ export const metadata = {
 
 export const revalidate = 3600;
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +25,12 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
