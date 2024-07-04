e202d492721ab69f899d0ae2c8f620a9c4b67ab9
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-07-04 19:40:56 +0700

    feat(guest): add feature update profile

--- 21-the-wild-oasis-website/app/_lib/action.js
+++ 21-the-wild-oasis-website/app/_lib/action.js
@@ -1,6 +1,22 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { updateGuest } from "./data-service";
+
+export async function updateProfileAction(formData) {
+  const session = await auth();
+  if (!session) throw new Error("Not authenticated");
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
+    throw new Error("Invalid national ID");
+  }
+  const updateData = { nationality, nationalID, countryFlag };
+  await updateGuest(session.user.guestId, updateData);
+
+  revalidatePath("/account/profile");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
